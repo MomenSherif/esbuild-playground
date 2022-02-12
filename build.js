ee46771687684fed1d6d96f4c71b874d2ece641e
Momen Sherif <momensherif.2019@gmail.com>
2022-02-12 20:42:10 +0200

    bundling for node & exclude external deps in bundle

--- build.js
+++ build.js
@@ -4,6 +4,15 @@ esbuild
   .build({
     entryPoints: ['src/app.jsx'],
     bundle: true,
-    outfile: 'dist/index.js',
+    sourcemap: true,
+    minify: true,
+    // Bundling for node
+    // platform: 'node', // will mark all built in node modules as externals to not be included in the bundle
+    // target: ['node10.4'],
+    target: ['chrome58', 'firefox57', 'safari11', 'edge16', 'ie11'],
+    // You can exclude all of your dependencies from the bundle by marking them as external (Good for libraries as dependencies will be downloaded when installing the package)
+    // external: ['./node_modules/*'],
+    // outfile: 'dist/index.js',
+    outdir: 'dist',
   })
   .catch(() => process.exit(1));
--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import server from 'react-dom/server';
 
+const one = { one: 1 };
+const two = { two: 2 };
+const three = { ...one, ...two, three: 3 };
+
 const Greet = () => <h1>Hello, World!</h1>;
 
 console.log(server.renderToString(<Greet />));
